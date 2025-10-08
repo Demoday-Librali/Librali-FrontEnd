e7595e4d16eb64ca195a898dbf0e36ad64536762
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-10-08 01:34:25 -0300

    Atulizando footer

--- src/Componente/ComponenteProps.jsx
+++ src/Componente/ComponenteProps.jsx
@@ -1,9 +1,14 @@
 import Style from './css/ComponenteProps.module.css';
 
-function ComponenteProps({Logo}) {
+function ComponenteProps({Logo, TikTok, Instagram, X, Facebook, YouTube}) {
     return (
         <div className={Style.box}>
             <img src={Logo} alt="Logo da Librali"  className={Style.image_props}/>
+            <img src={TikTok} alt="tiktok da Librali"  className={Style.TikTok_props}/>
+            <img src={Instagram} alt="Instagram da Librali"  className={Style.Instagram_props}/>
+            <img src={X} alt="X da Librali"  className={Style.X_props}/>
+            <img src={Facebook} alt="Facebook da Librali"  className={Style.Facebook_props}/>
+            <img src={YouTube} alt="YouTube da Librali"  className={Style.YouTube_props}/>
         </div>
     )
 }
--- src/Componente/Footer.jsx
+++ src/Componente/Footer.jsx
@@ -1,6 +1,11 @@
 import Style from '../Componente/css/Footer.module.css'
 import rodape from '../assets/img/Footer.png'
 import Logo from '../assets/img/Logo-escrita.png'
+import TikTok from '../assets/img/TikTok.png'
+import Instagram from '../assets/img/Instagram.png'
+import X from '../assets/img/Twitter.png'
+import Facebook from '../assets/img/Facebook.png'
+import YouTube from '../assets/img/YouTube.png'
 
 function Footer() {
     return (
@@ -13,13 +18,13 @@ function Footer() {
 
             {/* Aqui começa os links do footer */}
             <div id={Style.links_footer}>
-            <div class="Mapa">
+            <div className={Style.Mapa}>
                 <h3>Mapa do Site</h3>
                 <a href="#">Página Inicial</a>
                 <a href="#">Intérpretes</a>
                 <a href="#">Sobre</a>
             </div>
-            <div class="Ferramentas">
+            <div className={Style.Ferramentas}>
                 <h3>Ferramentas</h3>
                 <a href="#">Agendamento</a>
                 <a href="#">IA</a>
@@ -27,6 +32,14 @@ function Footer() {
             </div>
             {/* Aqui termina os links do footer */}
             {/* Aqui começa o código das redes sociais e contatos */}
+            <div id={Style.contator_footer}>
+                <h3>Siga nossas redes sociais</h3>
+                <a href='index.html'> <img src={TikTok} alt="Tiktok da Librali" /> </a>
+                <a href='#'> <img src={Instagram} alt="Instagram da Librali" /> </a>
+                <a href='#'> <img src={X} alt="X da Librali" /> </a>
+                <a href='#'> <img src={Facebook} alt="Facebook da Librali" /> </a>
+                <a href='#'> <img src={YouTube} alt="YouTube da Librali" /> </a>
+            </div>
             {/* Aqui termina o código das redes sociais e contatos */}
         </footer>
     )
