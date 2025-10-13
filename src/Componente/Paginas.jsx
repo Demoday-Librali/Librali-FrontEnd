6c0b1d71bfea9d7c989a7a414ad063bf46e1b811
unknown <iamcleyton@gmail.com>
2025-10-13 16:36:32 -0300

    subindo alteracoes

--- src/Componente/Paginas.jsx
+++ src/Componente/Paginas.jsx
@@ -2,7 +2,7 @@ import Styles from './css/Paginas.module.css'
 
 function Paginas() {
     return(
-    <section className={Styles.final}>
+    <section className={Styles.pagina_atual}>
         <p>Mostrando: 6 de 116</p>
     </section>
     )
