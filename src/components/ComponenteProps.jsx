cc17fdf76fa3d7c140a4770830bb724339e8e78f
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-10-25 09:08:19 -0300

    problema no Youtube do footer

--- src/components/ComponenteProps.jsx
+++ src/components/ComponenteProps.jsx
@@ -1,6 +1,6 @@
 import Style from './css/componenteProps.module.css';
 
-export default function componenteProps({Logo, TikTok, Instagram, X, Facebook, Youtube, Educacional, Juridico, Saude, Fundoia, Mulheria, Iconeia, Blocoverde, Categoria, Interprete, Cadastro, Impulso, Rosa, Laranja, Verde, Azul, Fundodepoimento, Cadeado, Check, Checkgif, Fundointerprete, Fundointerprete1, Interpreteuser, Ellipse, Mulherinterprete, Footer}) {
+export default function componenteProps({Logo, TikTok, Instagram, X, Facebook, YouTube, Educacional, Juridico, Saude, Fundoia, Mulheria, Iconeia, Blocoverde, Categoria, Interprete, Cadastro, Impulso, Rosa, Laranja, Verde, Azul, Fundodepoimento, Cadeado, Check, Checkgif, Fundointerprete, Fundointerprete1, Interpreteuser, Ellipse, Mulherinterprete, Footer}) {
     return (
         <div className={Style.box}>
             <img src={Logo} alt="Logo da Librali"  className={Style.image_props}/>
@@ -8,7 +8,7 @@ export default function componenteProps({Logo, TikTok, Instagram, X, Facebook, Y
             <img src={Instagram} alt="Instagram da Librali"  className={Style.Instagram_props}/>
             <img src={X} alt="X da Librali"  className={Style.X_props}/>
             <img src={Facebook} alt="Facebook da Librali"  className={Style.Facebook_props}/>
-            <img src={Youtube} alt="Youtube da Librali"  className={Style.YouTube_props}/>
+            <img src={YouTube} alt="Youtube da Librali"  className={Style.YouTube_props}/>
             <img src={Educacional} alt=""  className={Style.Educacional_props}/>
             <img src={Juridico} alt=""  className={Style.Juridico_props}/>
             <img src={Saude} alt=""  className={Style.Saude_props}/>
