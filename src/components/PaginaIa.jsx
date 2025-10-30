03edefa15760d573e46eeb4fca8be660b631695c
Kevin <kevin.ferreira0510@gmail.com>
2025-10-29 23:51:40 -0300

    responsibilidade ia <3

--- src/components/PaginaIa.jsx
+++ src/components/PaginaIa.jsx
@@ -15,9 +15,10 @@ function PaginaIa() {
             <h1 className={Style.Titulo}>Nome Pagína de IA</h1>
             <p className={Style.Texto1}>Já pensou em ter uma ajudinha extra na palma da sua mão? Nossa página de IA está aqui para tornar sua experiência mais simples, rápida e inteligente..</p>
             <p className={Style.Texto2}>Venha conhecer, interagir e descobrir como a tecnologia pode facilitar sua vida!</p>
-            <div id={Style.botoes}>
-                <button id={Style.cadastro}>Cadastro</button>
-                <button id={Style.login}>Login</button>
+            
+            <div className={Style.botoes}>
+                <button className={Style.cadastro}>Cadastro</button>
+                <button className={Style.login}>Login</button>
             </div>
 
         </div>
