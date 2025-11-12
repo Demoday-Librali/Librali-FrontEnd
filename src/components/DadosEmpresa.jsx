87a987d42887f07bfb10dbbc3b3f8f553a82f08f
May <mayar_234@outlook.com>
2025-11-12 15:06:15 -0300

    Alterações de estilo

---
+++ src/components/DadosEmpresa.jsx
@@ -0,0 +1,10 @@
+import Styles from './DadosEmpresa.module.css';
+
+function DadosEmpresa() {
+  return (
+    <div>
+      <h2>Dados da Empresa</h2>
+      <p>Informações sobre a empresa serão exibidas aqui.</p>
+    </div>
+  );
+}
--- src/components/PaginaIa.jsx
+++ src/components/PaginaIa.jsx
@@ -12,13 +12,13 @@ function PaginaIa() {
             <img src={Mulheria} alt="" className={Style.Mulheria} />
             <img src={Iconeia} alt="" className={Style.Iconeia} />
             <img src={Blocoverde} alt="" className={Style.Blocoverde} />
-            <h1 className={Style.Titulo}>Nome Pagína de IA</h1>
+            <h1 className={Style.Titulo}>Conheça a Lara</h1>
             <p className={Style.Texto1}>Já pensou em ter uma ajudinha extra na palma da sua mão? Nossa página de IA está aqui para tornar sua experiência mais simples, rápida e inteligente..</p>
             <p className={Style.Texto2}>Venha conhecer, interagir e descobrir como a tecnologia pode facilitar sua vida!</p>
             
             <div className={Style.botoes}>
-                <button className={Style.cadastro}>Cadastro</button>
-                <button className={Style.login}>Login</button>
+                <button className={Style.cadastro}>Explore</button>
+                <button className={Style.login}>Ver mais</button>
             </div>
 
         </div>
