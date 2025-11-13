5a231b3ee51849d2a6dee9fdfbb8c358f22c88f8
Eduxplorer <edu.santossilvadev@gmail.com>
2025-11-13 15:22:18 -0300

    Tela de cadastro empresa - foto

--- src/components/cadastro-empresas/EmpresaFoto.jsx
+++ src/components/cadastro-empresas/EmpresaFoto.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Styles from './css/EmpresaFoto.module.css'
+import company from '../../assets/images/foto-perfil-empresa-inicial.png'
 
 const EmpresaFoto = () => {
   return (
-    <div>
-        <h2>Empresa Foto</h2>
-    </div>
+    <div className={Styles.empresa}>
+            <h1>Cadastro Empresas</h1>
+            <div id={Styles.meio}>
+              <div className={Styles.form_control}>
+                  <div id={Styles.imagem_empresa}>
+                    <img src={company} alt="" id={Styles.image}/>
+                      <p>empresa</p>
+                  </div>
+              </div>
+              <div className={Styles.form_control}>
+                  <textarea
+                   type="text"
+                   name='nome'
+                   id='nome'
+                   placeholder='Descrição da empresa aqui'
+                   />
+              </div>
+            </div>
+        </div>
   )
 }
 
