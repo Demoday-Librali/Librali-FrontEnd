d1a471265f232f6174490bfc6ee03101c8cb282d
GabrielDamascenoAlmeida <gabrieldamascenoalmeida341@gmail.com>
2025-11-13 12:50:31 -0300

    Tela de cadastro dee empresa - endereço

--- src/components/cadastro-empresas/EmpresaEndereco.jsx
+++ src/components/cadastro-empresas/EmpresaEndereco.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
+import Styles from './css/EmpresaEndereco.module.css'
 
 const EmpresaEndereco = () => {
   return (
-    <div>
-        <h2>Empresa endereco</h2>
-    </div>
+    <div className={Styles.empresa}>
+                <h1>Cadastro Empresas</h1>
+                <div id={Styles.superior}>
+                  <div className={Styles.form_control}>
+                      <input
+                       type="number"
+                       name='cep'
+                       id={Styles.cep}
+                       placeholder='CEP'
+                       />
+                  </div>
+                  <div className={Styles.form_control}>
+                      <input
+                       type="number"
+                       name='numero'
+                       id={Styles.numero}
+                       placeholder='N°'
+                       />
+                  </div>
+                  <div className={Styles.form_control}>
+                      <input
+                       type="number"
+                       name='complemento'
+                       id={Styles.complemento}
+                       placeholder='Complemento'
+                       />
+                  </div>
+                </div>
+                <div id={Styles.meio}>
+                  <div className={Styles.form_control}>
+                      <input
+                        type="text"
+                        name='rua'
+                        id={Styles.rua}
+                        placeholder='Rua'
+                        />
+                  </div>
+                  <div className={Styles.form_control}>
+                      <input
+                        type="text"
+                        name='uf'
+                        id={Styles.uf}
+                        placeholder='UF'
+                        />
+                  </div>
+                </div>
+                <div className={Styles.form_control}>
+                    <input
+                      type="text"
+                      name='cidade'
+                      id='cidadee'
+                      placeholder='Cidade'
+                      />
+                </div>
+            </div>
   )
 }
 
