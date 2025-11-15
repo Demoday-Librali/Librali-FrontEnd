de558a1a6c0e9f6cd3536949168c314e514381ec
GabrielDamascenoAlmeida <gabrieldamascenoalmeida341@gmail.com>
2025-11-15 20:54:16 -0300

    80% da tela de cadastro de intérpretes feita

--- src/components/Cadastro-interpretes/InterpreteFoto.jsx
+++ src/components/Cadastro-interpretes/InterpreteFoto.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Styles from './css/InterpreteFoto.module.css'
+import imagem from '../../assets/images/perfilusuario.png'
 
 export default function InterpreteFoto() {
   return (
-    <div>
-        <h2>InterpreteFoto</h2>
+    <div className={Styles.interprete}>
+       <h1>Cadastro Intérprete</h1>
+                   <div id={Styles.meio}>
+                     <div className={Styles.form_control}>
+                         <div id={Styles.imagem_interprete}>
+                             <img src={imagem} alt="" id={Styles.image}/>
+                             <p>Escolha uma foto de perfil</p>
+                         </div>
+                     </div>
+                     <div className={Styles.form_control}>
+                         <textarea
+                          type="text"
+                          name='nome'
+                          id='nome'
+                          placeholder='Sua descrição aqui'
+                          />
+                     </div>
+      </div>
     </div>
   )
 }
