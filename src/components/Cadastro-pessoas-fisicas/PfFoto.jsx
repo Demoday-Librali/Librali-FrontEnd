c4bfe7423a8e3d70c9070653361f6c289bf172e9
GabrielDamascenoAlmeida <gabrieldamascenoalmeida341@gmail.com>
2025-11-15 23:32:38 -0300

    Finalização das telas de cadastro de pessoas físicas

--- src/components/Cadastro-pessoas-fisicas/PfFoto.jsx
+++ src/components/Cadastro-pessoas-fisicas/PfFoto.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Styles from './css/PfFoto.module.css'
+import imagem from '../../assets/images/perfilusuario.png'
 
 const PfFoto = () => {
   return (
-    <div>
-        <h2>PfFoto</h2>
-    </div>
+    <div className={Styles.pessoa_fisica}>
+           <h1>Cadastro Pessoa Física</h1>
+          <div id={Styles.meio}>
+            <div className={Styles.form_control}>
+                <div id={Styles.imagem_pf}>
+                    <img src={imagem} alt="" id={Styles.image}/>
+                    <p>Escolha uma foto de perfil</p>
+                </div>
+            </div>
+            <div className={Styles.form_control}>
+                <textarea
+                type="text"
+                name='nome'
+                id='nome'
+                placeholder='Sua descrição aqui'
+                />
+            </div>
+          </div>
+        </div>
   )
 }
 
--- src/components/Cadastro-pessoas-fisicas/PfPerfil.jsx
+++ src/components/Cadastro-pessoas-fisicas/PfPerfil.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
+import Styles from './css/PfPerfil.module.css'
 
 const PfPerfil = () => {
   return (
-    <div>
-        <h2>PfPerfil</h2>
-    </div>
+    <div className={Styles.pessoa_fisica}>
+                <h1>Cadastro Pessoa Física</h1>
+                <div className={Styles.form_control}>
+                    <input
+                     type="text" 
+                     name='nome_completo' 
+                     id='nome_completo' 
+                     placeholder='Nome completo' 
+                     />
+                </div>
+                <div className={Styles.form_control}>
+                    <input
+                     type="text" 
+                     name='cpf' 
+                     id='cpf' 
+                     placeholder='CPF' 
+                     />
+                </div>
+                <div id={Styles.meio}>
+                  <div className={Styles.form_control}>
+                      <select name="Genero" id="" placeholder="Genero">
+                          <option value="F">Feminino</option>
+                          <option value="M">Masculino</option>
+                          <option value="O">Outro</option>
+                      </select>
+                  </div>
+                  <div className={Styles.form_control}>
+                      <input 
+                      type="date" 
+                      name="data_nasc" 
+                      id={Styles.data_nasc} />
+                  </div>
+                </div>
+                <div className={Styles.form_control}> 
+                  <div className={Styles.senhas}>
+                    <div className={Styles.form_control}>
+                        <input
+                         type=""
+                         name='senha'
+                         id={Styles.form_senha}
+                         placeholder='Senha'
+                         />
+                    </div>
+                    <div className={Styles.form_control}>
+                        <input
+                         type="password"
+                         name='conf_senha'
+                         id={Styles.form_senha}
+                         placeholder='Confirmar senha'
+                         />
+                    </div>
+                  </div>
+                </div>
+            </div>
   )
 }
 
