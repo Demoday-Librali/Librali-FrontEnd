84f95cae9ec0351372ef21bd8f1fa64ceaabed3c
GabrielDamascenoAlmeida <gabrieldamascenoalmeida341@gmail.com>
2025-11-22 23:57:11 -0300

    Wizard Forms

--- src/components/CadastroOficial.jsx
+++ src/components/CadastroOficial.jsx
@@ -4,26 +4,29 @@ import CadastroPessoal from './cadastro-oficial/CadastroPessoal'
 import CadastroPerfil from './cadastro-oficial/CadastroPerfil'
 import CadastroFoto from './cadastro-oficial/CadastroFoto'
 import CadastroContato from './cadastro-oficial/CadastroContato'
+import Steps from './Steps'
 
 import { cadastroForm } from '../hooks/cadastroForm'
 
 import inscricao from '../assets/images/sinal-inscricao.png'
 import bem_vindo from '../assets/images/sinal-bem-vindo.png'
+import logo from '../assets/images/logo-completo.png'
 
 function CadastroOficial() {
-    const empresaComponents = [
+    const cadastroComponents = [
             <CadastroPerfil />,
             <CadastroPessoal />,
             <CadastroContato />,
             <CadastroFoto />
         ]
     
-    const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = cadastroForm(empresaComponents)
+    const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = cadastroForm(cadastroComponents)
 
   return (
     <div className={Styles.container_cadastro}>
         <div className={Styles.form_container}>
             <div className={Styles.conteudo_esquerda}>
+                <Steps currentStep={currentStep} />
                 <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
                     <h1>{currentComponent}</h1>
                     <div className={Styles.input_container}></div>
@@ -48,9 +51,11 @@ function CadastroOficial() {
             </div>
             {!isLastStep ? (
                 <div className={Styles.conteudo_direito} style={{backgroundImage: `url(${inscricao})`}}>
+                    <img src={logo} alt="Logotipo Librali" />
                 </div>
             ) : (
                 <div className={Styles.conteudo_direito} style={{backgroundImage: `url(${bem_vindo})`}}>
+                    <img src={logo} alt="Logotipo Librali" />
                 </div>
             ) }
         </div>
---
+++ src/components/Steps.jsx
@@ -0,0 +1,31 @@
+import { FaUser } from "react-icons/fa6";
+import { FaFingerprint } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { AiFillPicture } from "react-icons/ai";
+
+import './css/Steps.css'
+
+const Steps = ({currentStep}) => {
+  return (
+    <div className="steps">
+        <div className={`step ${currentStep >=1 ? 'active' : ''}`}>
+          <FaUser />
+            <p id="internop">Perfil</p>
+        </div>
+        <div className={`step ${currentStep >=2 ? 'active' : ''}`}>
+            <FaFingerprint />
+            <p id="internop">Pessoal</p>
+        </div>
+        <div className={`step ${currentStep >=3 ? 'active' : ''}`}>
+            <IoIosMail />
+            <p id="internop">Contato</p>
+        </div>
+        <div className={`step ${currentStep >=4 ? 'active' : ''}`}>
+            <AiFillPicture />
+            <p id="internop">Foto</p>
+        </div>
+    </div>
+  )
+}
+
+export default Steps
